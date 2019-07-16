a4345e5a44c6392928fbdd5b881c1552b8d8ed6c
eenriq5 <eenriqu5@asu.edu>
2019-07-16 07:28:17 -0700

    this is now running a very basic program now all we need to do is make a couple of different rountines

--- part2/thread_test.c
+++ part2/thread_test.c
@@ -1,3 +1,46 @@
 #include "threads.h"
 
-struct TCB_t RunQ
+
+
+int Global_Counter;
+
+void function1();
+void function2();
+void function3();
+
+int main(int argc , char** argv){
+
+    runQ = (struct Queue*)malloc(sizeof(struct Queue));//allocate the head pointer
+    InitQueue(runQ);//initilize the runQ pointer to NULL
+    start_thread(function1);
+    start_thread(function2);
+    start_thread(function3);
+    run();
+    return 0;
+
+}
+
+void function1(){
+    while(1){
+        printf("function1\n");
+        sleep(1);
+        yield();
+    }
+}
+
+void function2(){
+    while(1){
+        printf("function2\n");
+        sleep(1);
+        yield();
+    }
+}
+
+void function3(){
+    while (1){
+        printf("function3\n");
+        sleep(1);
+        yield();
+    }
+    
+}
--- part2/threads.h
+++ part2/threads.h
@@ -1,40 +1,41 @@
-#ifndef Threads_H
-#define Threads_H
 #include "q.h"
+#include <stdio.h>
+#define STACK_SIZE 8192
 
-struct Queue *runQ;
+/*global queue */
+struct Queue* runQ;
 
+void start_thread(void (*function)(void)){
+	void * stack = malloc(STACK_SIZE);
+	TCB_t * tcb = (TCB_t *) malloc(sizeof(TCB_t));
+	init_TCB(tcb, function, stack, STACK_SIZE);
+	
+	/* Add the TCB to the thread Q */
+	AddItem(runQ, tcb);
 
-void start_thread(void(*funtion)(void)){
-    void *stack = (void*)malloc(8192);
-    TCB_t *temp = NewItem();
-    init_TCB(temp,function,stack,8192);
-    AddItem(runQ, temp);
-    return;
 }
 
-void run() {
-	ucontext_t from, to;
+
+
+void run(){
 	ucontext_t parent;
-	from = parent;
 	getcontext(&parent);
-	to = (runQ->head->context);
 	swapcontext(&parent, &(runQ->head->context));
 
-	return;
-}
-
-void run() {
-	ucontext_t from, to;
-	ucontext_t parent;
-	from = parent;
-	getcontext(&parent);
-	to = (runQ->head->context);
-	swapcontext(&parent, &(runQ->head->context));
-
-	return;
 }
 
 
+void yield(){
+	ucontext_t *current, *next;
+	/* save the current context in the context struct
+		of the currently executing thread
+		(ie. the thread at the front of the runQ when yield is called == the tail after rotating)
+		and activate the context of the next thread
+		(ie. the thread at the front of th runQ after rotating) */
+	current = &runQ->head->context;
+	RotateQueue(runQ);
 
-#endif
+	/* next thread is now at head, because we rotated */
+	next = &runQ->head->context;
+	swapcontext(current, next);
+}
